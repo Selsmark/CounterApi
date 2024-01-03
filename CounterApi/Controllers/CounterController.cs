d6f0bc80e8823b30c44fbaf4b18500fa00e76b62
Selsmark <nick6659@zbc.dk>
2024-01-03 22:05:22 +0100

    Add project files.

---
+++ CounterApi/Controllers/CounterController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CounterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CounterController : ControllerBase
+    {
+        private readonly ILogger<CounterController> _logger;
+
+        public CounterController(ILogger<CounterController> logger)
+        {
+            _logger = logger;
+        }
+
+        private int counter = 0;
+
+        [HttpGet(Name = "Get")]
+        public int Get()
+        {
+            return counter;
+        }
+
+        [HttpGet(Name = "Increment")]
+        public int Increment()
+        {
+            return counter++;
+        }
+
+        [HttpGet(Name = "Decrement")]
+        public int Decrement()
+        {
+            return counter--;
+        }
+    }
+}
